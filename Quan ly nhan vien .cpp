b14a55822c8fc2d929e5d46733aa9f46c9aea049
SangHX <sanghoaxuan09@gmail.com>
2022-01-13 22:11:52 +0700

    Update Quan ly nhan vien .cpp

--- Quan ly nhan vien .cpp
+++ Quan ly nhan vien .cpp
@@ -2,3 +2,43 @@
 #include <conio.h>
 #include <string.h>
 
+struct NHANVIEN
+{
+    char cccd[15];//căn cước công dân
+    char phuong[100];// phường
+    char quan[50];//quận 
+    int tinhtrang; //tình trạng bệnh
+};
+
+void nhap1nv (NHANVIEN &nv)
+{   
+    fflush(stdin);
+    printf("\nNhap CCCD : ");
+    gets(nv.cccd);
+    fflush(stdin);
+    printf("\nNhap phuong : ");
+    gets(nv.phuong);
+    fflush(stdin);
+    printf("\nNhap quan : ");
+    gets(nv.quan);
+    printf("\nNhap tinh trang : ");
+    gets(nv.tinhtrang);
+}
+
+void nhapds(NHANVIEN ds[] , int &soluong)
+{
+    printf("\nNhap so luong nhan vien : ");
+    scanf("%d",&soluong);
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nNhap sinh vien thu %d ",i+1);
+        nhap1nv(ds[i]);
+    }
+    
+}
+int main()
+{
+    NHANVIEN ds[100];
+    int soluong;
+    nhapds(ds,soluong);
+}
